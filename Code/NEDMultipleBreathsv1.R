07f229c5f864374d33281b916459c9ad2da87ab7
GraceBurkholder <gkburkholder13@gmail.com>
2025-07-02 16:14:47 -0500

    "finished" for meeting with Dr. Schwartz

--- Code/NEDMultipleBreathsv1.R
+++ Code/NEDMultipleBreathsv1.R
@@ -6,8 +6,31 @@ rm(list=ls())
 library(tidyverse)
 library(janitor)
 
+
+#graphic settings
+lunair_palette=c(
+  "#6bdbdb","#143464", "#697e9c", "#ccd9e2", "#397b96")
+
+#extrafont::loadfonts()
+
+theme_lunair <- function(textsize=14){
+  theme_minimal() %+replace% 
+    theme(text = element_text(#family = "Arial", 
+      size = textsize), 
+      axis.ticks.length=unit(-0.05, "in"), 
+      axis.text.y.right = element_blank(), 
+      axis.text.x.top = element_blank(), 
+      axis.title.y.right = element_blank(),
+      axis.title.x.top = element_blank(),
+      #panel.border = element_rect(fill = NA),
+      plot.title = element_text(size = textsize,# face = "bold", 
+                                hjust = 0),
+      strip.background = element_rect(fill=NA))
+}
+
+
 #read in the CSV file with selected columns
-allNEDbreaths<-read.csv("data/NEDPractice5Breaths.csv", header=FALSE)
+allNEDbreaths<-read.csv("data/NEDPracticePx62Bailey.csv", header=FALSE)
 
 #make the header and remove automatic header rows and convert to numeric
 colnames(allNEDbreaths)<-as.character(unlist(allNEDbreaths[5, ]))
@@ -31,7 +54,8 @@ inspendindices<-which(grepl("InspEnd", allNEDbreaths$Comments))
 data_step<-0.0005
 
 #making an array to hold the area of each of the breaths
-allbreathareas<-vector(mode="numeric",length=length(inspendindices))
+allNEDbreathareas<-vector(mode="numeric",length=length(inspendindices))
+allregbreathareas<-vector(mode="numeric",length=length(inspendindices))
 
 #a for loop that will integrate from inspstart to inspend for the number of breaths selected
 for(i in 1:length(inspstartindices)){
@@ -51,10 +75,10 @@ for(i in 1:length(inspstartindices)){
   #making empty arrays to hold the areas
   midptNEDareas<-vector(mode="numeric",length=(end-start))
   leftareas<-vector(mode="numeric", length=vmax1index)
-  rightareas<-vector(mode="numeric", length=(length(breath)-vmax2index)
+  rightareas<-vector(mode="numeric", length=(length(breath)-first(vmax2index))) #one of the vmaxes has two back to back indices that have the same value
   
   #looping over the individual breath i to find midpt NED area
-   for(j in 1:length(breath)-1){
+   for(j in 1:(length(breath)-1)){
      #get the two values of y 
      base1=breath[j]
      base2=breath[j+1]
@@ -68,7 +92,7 @@ for(i in 1:length(inspstartindices)){
    }
  
    #store the sum of the NED midpoint areas in another vector
-  sum(midptNEDareas)->allbreathareas[i]
+  sum(midptNEDareas)->allNEDbreathareas[i]
   
   #looping over the individual breath i to find rectangle non-NED area
   #integrating the first segment with a midpt sum
@@ -90,7 +114,7 @@ for(i in 1:length(inspstartindices)){
     middlearea = (vmax2index-vmax1index)*data_step*vmax1
   
   #integrating the last segment with a trap sum
-  for (m in vmax2index:length(breath)-1){
+  for(m in 1:(length(breath)-vmax2index-1)){
     #get the two values of y 
     base1=breath[m]
     base2=breath[m+1]
@@ -104,7 +128,23 @@ for(i in 1:length(inspstartindices)){
   }
     rightsum<-sum(rightareas)
     
+    #summing the left, middle, and right and storing that value in the array for each i
     TVnonNED<-leftsum+middlearea+rightsum
+    sum(TVnonNED)->allregbreathareas[i]
     
-    TVNED <-sum(allbreathareas)
 }
+
+##comparing NED and nonNED breaths
+
+percentNEDoverIdeal<-(allNEDbreathareas/allregbreathareas)*100
+
+
+#plot the waveform to see the breaths
+
+time=allNEDbreaths$`ChannelTitle=`
+flow=allNEDbreaths$`Flow Sensor Total`
+NEDPlot<-ggplot(data = allNEDbreaths, aes(x=time, y=flow))+
+  geom_line(color=lunair_palette[2], linewidth=0.25)+
+  theme_lunair()
+NEDPlot
+
